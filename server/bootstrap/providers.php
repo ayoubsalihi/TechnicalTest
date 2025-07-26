0ad8c727fbe1eddc1d43a182204e9e8bf057095c
ayoubsalihi <ayoubsqlehi50@gmail.com>
2025-07-26 17:15:25 +0100

    Globalisation des providers

--- server/bootstrap/providers.php
+++ server/bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\RepositoryServiceProvider::class,
+    App\Providers\RepositoryServiceProvider::class,
+    Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 ];
